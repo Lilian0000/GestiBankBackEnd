f89de2cd6dc37ccf0e8d84ec67646c091d7d1c1b
Lilian0000 <arrlil93@gmail.com>
2018-06-29 00:14:48 +0200

    Dépot back end GestiBank

---
+++ src/main/java/com/wha/controller/ClientRestController.java
@@ -0,0 +1,67 @@
+package com.wha.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wha.dao.ClientDao;
+import com.wha.model.Client;
+
+@RestController
+public class ClientRestController {
+	private ClientDao clientDAO;
+
+	public ClientRestController() {
+		this.clientDAO = new ClientDao();
+	}
+
+	@GetMapping("/clients")
+	public List getClients() {
+		return clientDAO.list();
+	}
+
+	@GetMapping("/clients/{id}")
+	public ResponseEntity<Client> getClient(@PathVariable("id") Long id) {
+
+		Client client = clientDAO.get(id);
+
+		if (null == client) {
+			return new ResponseEntity<Client>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Client>(client, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/clients")
+	public ResponseEntity<Client> createCustomer(@RequestBody Client client){
+		
+		clientDAO.create(client);
+		return new ResponseEntity<Client>(client, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/clients/{id}")
+	public ResponseEntity<Long> deleteClient(@PathVariable Long id) {
+
+		if (null == clientDAO.delete(id)) {
+			return new ResponseEntity<Long>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Long>(id, HttpStatus.OK);
+	}
+	
+	@PutMapping("/clients/{id}")
+	public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+		client = clientDAO.update(id, client);
+
+		if (null == client) {
+			return new ResponseEntity<Client>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Client>(client, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/wha/model/Client.java
@@ -0,0 +1,87 @@
+package com.wha.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Client {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobile;
+	private Date dateOfBirth;
+	private List<Compte> comptes;
+
+	public List<Compte> getComptes() {
+		return comptes;
+	}
+
+	public void setComptes(List<Compte> comptes) {
+		this.comptes = comptes;
+	}
+
+	public Client(Long id, String firstName, String lastName, String email, String mobile, List<Compte> comptes) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobile = mobile;
+		this.dateOfBirth = new Date();
+		this.comptes = comptes;
+	}
+
+	public Client() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+}
